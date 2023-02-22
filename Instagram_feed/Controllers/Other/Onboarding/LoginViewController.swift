8e11f279808d6e27ef530b9ababd3f57ebda38fb
Your Name <you@example.com>
2023-02-22 15:42:58 -0800

    include login ui elements

--- Instagram_feed/Controllers/Other/Onboarding/LoginViewController.swift
+++ Instagram_feed/Controllers/Other/Onboarding/LoginViewController.swift
@@ -8,14 +8,77 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let usernameEmailField: UITextField = {
+        return UITextField()
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+          let button = UIButton()
+        button.backgroundColor = .systemGray4
+        return button
+    }()
+    
+    
+    private let createAccountButton: UIButton = {
+          let button = UIButton()
+        button.backgroundColor = .systemGray4
+        return button
+    }()
+    
+    
+    private let termsButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let privacyButton: UIButton = {
+          let button = UIButton()
+        button.backgroundColor = .systemGray4
+        return button
+    }()
+    
+    private let headerView: UIView = {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        return UIView()
+    }()
+    
+    
+  
 
-        view.backgroundColor = .systemGray4
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //assign frames
+
+        view.backgroundColor = .systemBackground
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func addSubviews(){
+        view.addSubview(usernameEmailField)
+        view.addSubview(passwordField)
+        view.addSubview(loginButton)
+        view.addSubview(createAccountButton)
+        view.addSubview(termsButton)
+        view.addSubview(privacyButton)
+        view.addSubview(headerView)
+    }
 
-
+    @objc private func didTapLoginButton(){}
+    @objc private func didTapTermsButton(){}
+    @objc private func didTapPrivacyButton(){}
+    @objc private func didTapCreateAccountButton(){}
 
 }
